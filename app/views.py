be172d501a02fa4438bbaca9557cd0004af6cc0d
WassCodeur <wachioubouraima56@gmail.com>
2024-02-23 21:23:48 +0000

    Update navbar in base.html and add home.html template

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template('base.html')
+    return render_template('home.html')
 
 @views.route('/login')
 def login():
